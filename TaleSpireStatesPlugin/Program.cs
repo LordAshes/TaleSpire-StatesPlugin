784d9b5bfe3491d2bc4128ec6fa6b0bcfcc25024
Lord Ashes <webtable@gmail.com>
2021-06-08 09:34:39 -0400

    Rewrote to use Stat Messaging
    Rewrote to use Stat Messaging for higher compatibility with other plugins that use Creature Name for synchronization

--- TaleSpireStatesPlugin/Program.cs
+++ TaleSpireStatesPlugin/Program.cs
@@ -13,12 +13,13 @@ using Newtonsoft.Json;
 namespace LordAshes
 {
     [BepInPlugin(Guid, Name, Version)]
+    [BepInDependency(LordAshes.StatMessaging.Guid)]
     public class StatesPlugin : BaseUnityPlugin
     {
         // Plugin info
         public const string Name = "States Plug-In";
         public const string Guid = "org.lordashes.plugins.states";
-        public const string Version = "1.1.0.0";
+        public const string Version = "2.0.0.0";
 
         // Configuration
         private ConfigEntry<KeyboardShortcut> triggerKey { get; set; }
@@ -44,7 +45,7 @@ namespace LordAshes
             triggerKey = Config.Bind("Hotkeys", "States Activation", new KeyboardShortcut(KeyCode.S, KeyCode.LeftControl));
             baseColor = Config.Bind("Appearance", "Base Text Color", UnityEngine.Color.black);
 
-            if(System.IO.File.Exists(dir+"Config/"+Guid+"/ColorizedKeywords.json"))
+            if (System.IO.File.Exists(dir + "Config/" + Guid + "/ColorizedKeywords.json"))
             {
                 string json = System.IO.File.ReadAllText(dir + "Config/" + Guid + "/ColorizedKeywords.json");
                 colorizations = JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
@@ -57,21 +58,87 @@ namespace LordAshes
         /// </summary>
         void Update()
         {
-            if(isBoardLoaded())
+            if (isBoardLoaded())
             {
+                StatMessaging.Check(HandleRequest);
+
+                SyncStealthMode();
+
                 if (triggerKey.Value.IsUp())
                 {
-                    SetNameRequest();
+                    SetRequest();
                 }
+            }
+        }
 
-                CheckNameRequest();
+        public void HandleRequest(StatMessaging.Change[] changes)
+        {
+            foreach (StatMessaging.Change change in changes)
+            {
+                if (change.key == StatesPlugin.Guid)
+                {
+                    try
+                    {
+                        CreatureBoardAsset asset;
+                        CreaturePresenter.TryGetAsset(change.cid, out asset);
+                        if (asset != null)
+                        {
+                            TextMeshPro creatureStateText = null;
+                            switch (change.action)
+                            {
+                                case StatMessaging.ChangeType.added:
+                                case StatMessaging.ChangeType.modified:
+                                    GameObject creatureBlock = GameObject.Find(asset.Creature.CreatureId + ".StatesBlock");
+                                    if (creatureBlock == null)
+                                    {
+                                        Debug.Log("Creating CreatureBlock GameObject");
+                                        creatureBlock = new GameObject(asset.Creature.CreatureId + ".StatesBlock");
+                                        creatureBlock.transform.position = asset.BaseLoader.LoadedAsset.transform.position;
+                                        creatureBlock.transform.rotation = Quaternion.Euler(0, 0, 0);
+                                        creatureBlock.transform.SetParent(asset.BaseLoader.LoadedAsset.transform);
+
+                                        Debug.Log("Creating TextMeshPro");
+                                        creatureStateText = creatureBlock.AddComponent<TextMeshPro>();
+                                        creatureStateText.transform.position = creatureBlock.transform.position;
+                                        creatureStateText.transform.rotation = creatureBlock.transform.rotation;
+                                        creatureStateText.textStyle = TMP_Style.NormalStyle;
+                                        creatureStateText.enableWordWrapping = true;
+                                        creatureStateText.alignment = TextAlignmentOptions.Center;
+                                        creatureStateText.autoSizeTextContainer = true;
+                                        creatureStateText.color = baseColor.Value;
+                                        creatureStateText.fontSize = 1;
+                                        creatureStateText.fontWeight = FontWeight.Bold;
+                                    }
+                                    Debug.Log("Populating TextMeshPro");
+                                    creatureStateText.autoSizeTextContainer = false;
+                                    string content = change.value.Replace(",", "\r\n");
+                                    if (colorizations.ContainsKey("<Default>")) { content = "<Default>" + content; }
+                                    creatureStateText.richText = true;
+                                    foreach (KeyValuePair<string, string> replacement in colorizations)
+                                    {
+                                        content = content.Replace(replacement.Key, replacement.Value);
+                                    }
+                                    creatureStateText.text = content;
+                                    int lines = content.Split('\r').Length;
+                                    creatureStateText.transform.position = new Vector3(creatureBlock.transform.position.x, 1.25f + (0.1f * lines), creatureBlock.transform.position.z);
+                                    creatureStateText.autoSizeTextContainer = true;
+                                    break;
+                                case StatMessaging.ChangeType.removed:
+                                    Debug.Log("Removing States Block for creature '" + change.cid + "'");
+                                    GameObject.Destroy(GameObject.Find(asset.Creature.CreatureId + ".StatesBlock"));
+                                    break;
+                            }
+                        }
+                    }
+                    catch(Exception) { ; }
+                }
             }
         }
 
         /// <summary>
         /// Method to write stats to the Creature Name
         /// </summary>
-        public void SetNameRequest()
+        public void SetRequest()
         {
             Debug.Log("Setting asset...");
             CreatureBoardAsset asset;
@@ -79,102 +146,41 @@ namespace LordAshes
             if (asset != null)
             {
                 Debug.Log("Got selected asset...");
-                string stats = asset.Creature.Name;
-                if (!stats.Contains("|"))
+                string states = StatMessaging.ReadInfo(asset.Creature.CreatureId, StatesPlugin.Guid);
+
+                SystemMessage.AskForTextInput("State", "Enter Creature State(s):", "OK", (newStates) =>
                 {
-                    if (!stats.Contains(":"))
-                    {
-                        stats = stats + " ||";
-                    }
-                    else
-                    {
-                        stats = stats.Substring(0, stats.IndexOf(":")) + " || : " + stats.Substring(stats.IndexOf(":") + 1);
-                    }
-                }
-                string[] statParts = stats.Split('|');
-                SystemMessage.AskForTextInput("State", "Enter Creature State(s):", "OK", (s) =>
-                {
-                    CreatureManager.SetCreatureName(asset.Creature.CreatureId, statParts[0] + " |"+ s +"| " + statParts[2]);
+                    StatMessaging.SetInfo(asset.Creature.CreatureId, StatesPlugin.Guid, newStates);
                 },
-                null, "Clear", ()=>
+                null, "Clear", () =>
                 {
-                    CreatureManager.SetCreatureName(asset.Creature.CreatureId, statParts[0] + " || " + statParts[2]);
-                }, 
-                statParts[1].Replace("\r\n",","));
+                    StatMessaging.ClearInfo(asset.Creature.CreatureId, StatesPlugin.Guid);
+                },
+                states);
             }
         }
 
-        public void CheckNameRequest()
+        public void SyncStealthMode()
         {
-            foreach(CreatureBoardAsset asset in CreaturePresenter.AllCreatureAssets)
+            foreach (CreatureBoardAsset asset in CreaturePresenter.AllCreatureAssets)
             {
-
                 // Sync Hidden Status
-                GameObject block = GameObject.Find(asset.Creature.CreatureId + ".StatesBlock");
-                if (block != null)
+                try
                 {
-                    if (asset.Creature.IsExplicitlyHidden == true && block.GetComponent<TextMeshPro>().enabled == true)
+                    GameObject block = GameObject.Find(asset.Creature.CreatureId + ".StatesBlock");
+                    if (block != null)
                     {
-                        block.GetComponent<TextMeshPro>().enabled = false;
-                    }
-                    else if (asset.Creature.IsExplicitlyHidden == false && block.GetComponent<TextMeshPro>().enabled == false)
-                    {
-                        block.GetComponent<TextMeshPro>().enabled = true;
+                        if (asset.Creature.IsExplicitlyHidden == true && block.GetComponent<TextMeshPro>().enabled == true)
+                        {
+                            block.GetComponent<TextMeshPro>().enabled = false;
+                        }
+                        else if (asset.Creature.IsExplicitlyHidden == false && block.GetComponent<TextMeshPro>().enabled == false)
+                        {
+                            block.GetComponent<TextMeshPro>().enabled = true;
+                        }
                     }
                 }
-
-                // Look For Text Changes
-                string stats = asset.Creature.Name;
-                if (stats.Contains("|"))
-                {
-                    string[] statParts = asset.Creature.Name.Split('|');
-                    if (!creatureStates.ContainsKey(asset.Creature.CreatureId)) { creatureStates.Add(asset.Creature.CreatureId, ""); }
-                    if(statParts[1]!= creatureStates[asset.Creature.CreatureId])
-                    {
-                        creatureStates[asset.Creature.CreatureId] = statParts[1];
-                        Debug.Log("Accessing CreatureBlock GameObject");
-                        GameObject creatureBlock = GameObject.Find(asset.Creature.CreatureId+".StatesBlock");
-                        TextMeshPro creatureStateText = null;
-                        if (creatureBlock == null)
-                        {
-                            Debug.Log("Creating CreatureBlock GameObject");
-                            creatureBlock = new GameObject(asset.Creature.CreatureId + ".StatesBlock");
-                            creatureBlock.transform.position = new Vector3(asset.BaseLoader.transform.position.x+0.25f, asset.BaseLoader.transform.position.y + 1f, asset.BaseLoader.transform.position.z);
-                            creatureBlock.transform.rotation = Quaternion.Euler(10,10,-10);
-                            creatureBlock.transform.SetParent(asset.BaseLoader.transform);
-                            Debug.Log("Creating TextMeshPro");
-                            creatureStateText = creatureBlock.AddComponent<TextMeshPro>();
-                            creatureStateText.transform.position = creatureBlock.transform.position;
-                            creatureStateText.transform.rotation = creatureBlock.transform.rotation;
-                            creatureStateText.textStyle = TMP_Style.NormalStyle;
-                            creatureStateText.enableWordWrapping = true;
-                            creatureStateText.alignment = TextAlignmentOptions.Center;
-                            creatureStateText.autoSizeTextContainer = true;
-                            creatureStateText.color = baseColor.Value;
-                            creatureStateText.fontSize = 1;
-                            creatureStateText.fontWeight = FontWeight.Bold;
-                        }
-                        else
-                        {
-                            Debug.Log("Accessing TextMeshPro for BaseLoader");
-                            creatureStateText = creatureBlock.GetComponent<TextMeshPro>();
-                        }
-                        Debug.Log("Populating TextMeshPro");
-                        creatureStateText.autoSizeTextContainer = false;
-                        statParts[1] = statParts[1].Replace(",", "\r\n");
-                        if (colorizations.ContainsKey("<Default>")) { statParts[1] = "<Default>" + statParts[1]; }
-                        creatureStateText.richText = true;
-                        foreach (KeyValuePair<string, string> replacement in colorizations)
-                        {
-                            statParts[1] = statParts[1].Replace(replacement.Key, replacement.Value);
-                        }
-                        creatureStateText.text = statParts[1];
-                        int lines = statParts[1].Split('\r').Length;
-                        creatureStateText.transform.position = new Vector3(creatureBlock.transform.position.x, 1.25f+(0.1f*lines), creatureBlock.transform.position.z);
-                        creatureStateText.autoSizeTextContainer = true;
-
-                    }
-                }
+                catch(Exception) { ; }
             }
         }
 
